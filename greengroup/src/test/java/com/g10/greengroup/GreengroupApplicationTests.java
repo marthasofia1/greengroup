5c4410c67e2444f248ea6c19a2f74085e9c8efbe
Martha_Sofia <marthau85@hotmail.com>
2021-10-27 12:34:39 -0500

    visual code y mysql error al instalar tomcat comando mvnw.cmd spring-boot:run
    Signed-off-by: Martha_Sofia <marthau85@hotmail.com>

---
+++ greengroup/src/test/java/com/g10/greengroup/GreengroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g10.greengroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreengroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
